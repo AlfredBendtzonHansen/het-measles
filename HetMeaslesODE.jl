50971ad22e48ffb786a1f447afbf3e65dbd6ea45
Alfred <alfredbendtzonhansen@egmail.com>
2024-11-27 14:25:40 +0100

    Cleaner Code for HetMealesODE.jl and todo cont.

--- HetMeaslesODE.jl
+++ HetMeaslesODE.jl
@@ -66,31 +66,6 @@ function final_size(Nc, Ncs, H, β, ν, I0)
     return sum(sol.u[size(sol.u)[1]][3,:]-sol.u[1][3,:])/(Nc-sum(sol.u[1][3,:]))
 end
 
-H_res = 1000
-H_range = (0:1/H_res:1)
-Nc = 1000
-Nc_max = 100
-Ncs_range = 1:Nc_max
-
-I0_vec = zeros(Nc)
-I0_vec[1:2] .= 0.001
-#And the rest are connected to 1/(Nc-1) of the initally infected 
-I0_vec[3:end] .= 2*0.001/(Nc-1)
-
-final_sizes =zeros(H_res, Nc_max)
-
-
-β = 15*0.8
-ν = 0.8
-@showprogress for i in 1:H_res
-    for j in 1:Nc_max
-        final_sizes[i,j] = final_size(Nc, j, H_range[i], β, ν, I0_vec)
-    end 
-end 
-
-
-heatmap(final_sizes, title = "R0 = $(β/ν)")
-
 function analytic_R0(H, Ns, Nc, β, ν)
     Sc = Ns/Nc
     R0s = β/ν * (Ns+(Nc-2)*(1-H+H*Sc))/(2*(Nc-1))
@@ -99,43 +74,37 @@ function analytic_R0(H, Ns, Nc, β, ν)
     return max(R0s,R0i)
 end 
 
+β = 2*0.8
+ν = 0.8
+
+H_res = 1000
+H_range = (0:1/H_res:1)
+Nc = 10
+Nc_max = Nc#50
+Ncs_range = 1:Nc_max
+
+I0_vec = zeros(Nc)
+I0_vec[1:2] .= 0.001
+#And the rest are connected to 1/(Nc-1) of the initally infected 
+I0_vec[3:end] .= 2*0.001/(Nc-1)
+
+final_sizes =zeros(H_res, Nc_max)
 r0_calc = zeros(H_res,Nc_max)
-
-# r0_highres_calc = zeros(10000,10000)
-
-# h_range = 0:1/10000:1
-
-# @showprogress for i in 1:10000
-#     for j in 1:10000
-#         r0_highres_calc[i,j] = analytic_R0(h_range[i], j,Nc, β, ν)
-#     end 
-# end 
-# s = range(0,1,length = 10000)
-# H = range(0,1,length = 10000)
-# contour(H,s[1:100],r0_highres_calc[:,1:100], levels = 1:1,c =:red)
 
 
 @showprogress for i in 1:H_res
-    for j in Ncs_range
+    for j in 1:Nc_max
+        final_sizes[i,j] = final_size(Nc, j, H_range[i], β, ν, I0_vec)
         r0_calc[i,j] = analytic_R0(H_range[i], j,Nc, β, ν)
     end 
 end 
 
-function sq(ns)
-    #return 1-sqrt(1-ns)
-    return 2*ns-ns^2
-end
-
-function h_parm(H,sc)
-    return (H+sc-1)/(2*sc-1)
-end
-
-
-s = range(0,1,length = 100)
-H = range(0,1,length = 100)
+#Plotting
+s = range(0,1,length = Nc_max)
+H = range(0,1,length = H_res)
 
 heatmap(s,H,final_sizes, title = "β/ν = $(β/ν)",xlabel=L"s", ylabel=L"H", colorbar_title = L"Final\,\,\,Size")
-contour!(H,s,r0_calc/1000, levels = 1/1000:1/1000,c =:red)
+contour!(s,H,r0_calc/1000, levels = 1/1000:1/1000,c =:red)
 annotate!(0.1, 0.9, text(L"R_0(s,H) = 1", :red, 8))
 
 vline!([ν/β], color=:blue, linewidth=2,label=L"ν/β",legend = :bottomright)
@@ -145,3 +114,4 @@ vline!([0.043], color=:blue, linewidth=2,label=L"NE_data",legend = :bottomright)
 
 
 
+#ToDo, make sucetiibles fractional in intial also, so we just fill up from 1 to Nc compatmnets, and the randomly chooce and reorder suceptibles 
